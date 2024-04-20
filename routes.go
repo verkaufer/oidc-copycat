6c2c4bc70be04b817edd68ae796130e4a61a1e6c
David Gunter <david@davidgunter.ca>
2024-04-20 10:11:44 -0700

    Implement basic inheritable templates

--- routes.go
+++ routes.go
@@ -11,6 +11,8 @@ import (
 func registerRoutes(r *gin.Engine) {
 	r.GET("/", handleHealthCheck)
 	r.GET("/admin", handleAdminIndex)
+	r.GET("/admin/applications", handleAdminApplications)
+	r.GET("/admin/users", handleDirectoryList)
 
 	oauth2.RegisterHandlers(r)
 }
@@ -20,7 +22,19 @@ func handleHealthCheck(c *gin.Context) {
 }
 
 func handleAdminIndex(c *gin.Context) {
-	c.HTML(http.StatusOK, "admin/index.tmpl", gin.H{
+	c.HTML(http.StatusOK, "admin-index.html", gin.H{
 		"title": "Admin Panel",
+	})
+}
+
+func handleAdminApplications(c *gin.Context) {
+	c.HTML(http.StatusOK, "admin-applications.html", gin.H{
+		"title": "Applications",
+	})
+}
+
+func handleDirectoryList(c *gin.Context) {
+	c.HTML(http.StatusOK, "admin-directory.html", gin.H{
+		"title": "User Directory",
 	})
 }
